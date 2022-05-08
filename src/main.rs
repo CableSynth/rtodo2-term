a950fd27c16b1b5b5e58c0961438086f73c9df4d
CableSynth <lorencalebbaum@gmail.com>
2022-05-08 09:50:34 -0400

    add dir building

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use serde::{Deserialize, Serialize};
 
 use clap::{Parser, Subcommand};
-use std::{cell::Cell, collections::LinkedList};
+use std::env;
+use std::{
+    cell::Cell,
+    collections::LinkedList,
+    fs::{self, DirBuilder},
+    path,
+};
+
+const TODO_FILE: &str = "~/.rtodo2/todo_file";
 
 #[derive(Debug, Serialize, Deserialize, Copy, Clone)]
 enum Status {
@@ -62,10 +70,42 @@ struct Cli {
 }
 
 #[derive(Debug, Subcommand)]
-enum Commands {}
+enum Commands {
+    /// New todo
+    #[clap(arg_required_else_help = true)]
+    New {
+        /// The text for the TODO
+        #[clap(required = true)]
+        todo_str: String,
+    },
+
+    /// List todos
+    #[clap(arg_required_else_help = true)]
+    List,
+
+    /// Edit todo
+    #[clap(arg_required_else_help = true)]
+    Edit {
+        /// Index of the
+        index: i16,
+    },
+
+    /// Remove todo
+    #[clap(arg_required_else_help = true)]
+    Remove {
+        /// Index of the todo
+        index: i16,
+    },
+}
 
 fn main() {
-    let args = Cli::parse();
+    let path_name = shellexpand::full(TODO_FILE).unwrap();
+    let path = path::Path::new(path_name.as_ref());
+    let prefix = path.parent().unwrap();
 
-    print!("{:?}", args);
+    DirBuilder::new().recursive(true).create(prefix).unwrap();
+
+    // let args = Cli::parse();
+
+    print!("{:?}", prefix);
 }
